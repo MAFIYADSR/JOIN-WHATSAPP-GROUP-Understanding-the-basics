b496ee4a9209961daf2d2a6259d57c93dec22086
Akash <babaidutta222001@gmail.com>
2023-07-09 12:08:38 +0530

    Request / Response

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) =>
-{
-    res.end("Akash dutta");
+// const server = http.createServer((req, res) =>
+// {
+//     res.end("Akash dutta");
+// })
+
+// server.listen(4000)
+
+const http = require('http');
+const port = 4000;
+const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1> Akash dutta </h1>')
 })
 
-server.listen(4000)
+server.listen(port, ()=>{
+    console.log(`Server is listining on port ${port}`);
+})
